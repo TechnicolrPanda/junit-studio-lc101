1b8495e22c9ad51f6bc7b0e863da54fbea8c239d
TechnicolrPanda <amelia.a.fitzpatrick@gmail.com>
2021-05-27 11:36:21 -0500

    Wrote Out Tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -8,8 +8,63 @@ public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void bracketLaunchCodeBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void bracketLaunchBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launch]Code"));
+    }
+
+    @Test
+    public void bracketCodeBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void bracketBeforeBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void bracketLastBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCod[e]"));
+    }
+
+    @Test
+    public void bracketLaunchCodeBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode[]"));
+    }
+
+    @Test
+    public void mismatchedBrachetsReturnsFanse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void singleOpenBracketFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void singleClosedBracketFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void extraClosedBracketFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code]]"));
+    }
+
+    @Test
+    public void extraCodeBracketFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode]"));
     }
 
 
